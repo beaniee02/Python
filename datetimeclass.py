1bb5f0e93458d7452b1defadd8ada4feb7c2c8a1
Busolami Adesina <busolamitemiloluwa02@gmail.com>
2024-10-30 16:07:53 +0100

    datetime module

---
+++ datetimeclass.py
@@ -0,0 +1,60 @@
+import datetime as dt
+
+#Python DateTime
+
+# tim = dt.datetime.now()
+# print(tim)
+# print(tim.date())
+# print(tim.time())
+# print(tim.year)
+# print(tim.month)
+# print(tim.day)
+# print(tim.hour)
+# print(tim.minute)
+# print(tim.second)
+
+# tm = dt.datetime(2023, 4, 10, 12, 00)
+# print(tm)
+# print(tm.month)
+# tm = dt.datetime(2023, 4, 10, 12, 30, 20)
+# print(tm)
+
+# check = int(dt.datetime.now().strftime("%M"))
+# rang = []
+# [rang.append(rn) for rn in range(0,60)]
+# while True:
+#     time = dt.datetime.now()
+#     nexttime = time.strftime0('%M')
+#     if int(nexttime) in [rn for rn in range(0,60)] and check == int(nexttime):
+#         for i in range(5):
+#             print('I am going to school today')
+#     check = int(nexttime) + 1
+
+# sec = 00
+# while True:
+#     dtim = dt.datetime.now()
+#     day = dtim.strftime('%A')
+#     hour = dtim.strftime("%I")
+#     minute = dtim.strftime("%M")
+#     second = dtim.strftime("%S")
+
+#     if day == 'wednesday':
+#         if hour == 9 and minute == 54 and second == 00:
+#             for i in range(1):
+#                 print('You have class now')
+
+#     elif day == 'tuesday':
+#         print('ok')
+
+#python math class
+# import math, cmath
+# I = [2, 4, 5, 7, 3]
+# print(min(I))
+# print(max(I))
+# print(abs(-5.34))
+# print(pow(5, 3))
+# print(math.sqrt(9))
+# print(math.ceil(6.3492))
+# print(math.floor(5.6732))
+# print(math.pi)
+# print(math.pi * 1000 + 25)
