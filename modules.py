413ba602feeda315757469d796fa304d83b4ea98
Busolami Adesina <busolamitemiloluwa02@gmail.com>
2024-10-31 10:23:28 +0100

    modules note

---
+++ modules.py
@@ -0,0 +1,11 @@
+#modules are files that contain python codes it can be inbuilt or self-built. A module should contain similar function and class.o
+
+import random
+list_item = ['temi', 'taiwo', 'excel', 'tkristi']
+leader = random.choice(list_item)
+print(leader)
+
+
+
+
+#a library is a collection of several modules
